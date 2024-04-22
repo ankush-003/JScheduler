f749f596661978a7675b005dcc0cfb1bfe07fac5
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2024-04-23 01:14:03 +0530

    spring init

---
+++ src/main/java/com/ankush003/JScheduler/JSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.ankush003.JScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JSchedulerApplication.class, args);
+	}
+
+}
